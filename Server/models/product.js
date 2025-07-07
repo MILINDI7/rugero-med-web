a4277da2c3f03898a08594754ff18b6f57b5931c
Fabrice <f.nkurunziz@alustudent.com>
2025-07-07 10:17:46 +0200

    define a product model

---
+++ Server/models/product.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema(
+	{
+		title: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+
+		description: {
+			type: String,
+			minlength: 6,
+		},
+
+		category: {
+			type: String,
+			enum: [
+				'CSSD',
+				'Hospital Design',
+				'Plastic Surgery',
+				'Neurosurgery',
+				'Theatre',
+				'Home Care',
+			],
+		},
+
+		imageUrl: {
+			type: String,
+			required: true,
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+const Product = mongoose.model('Product', productSchema);
+
+export default Product;
